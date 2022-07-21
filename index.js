a7a296edfcf4eddfa859b171cbef72620fc6caca
Ben Greenberg <ben@bensinbox.com>
2022-07-21 14:03:08 -0400

    Ignore repeat requests

--- index.js
+++ index.js
@@ -22,46 +22,83 @@ exports.testSheets = functions.https.onRequest( async ( request, response ) => {
 exports.getIssueResponse = functions.https.onRequest( async ( request, response ) => {
   try {
     let request_body = request.body;
-    functions.logger.info("Received Request!", { structuredData: true });
-    functions.logger.info('request body', request_body, { structuredData: true });
+    functions.logger.info("Received Request!" );
+    functions.logger.info('request.headers', request.headers );
+    functions.logger.info('request body', request_body );
+
     if ( request_body.type && request_body.type == 'url_verification' ) {
+      console.log( 'Received url_verification request, sending challenge back', request_body.challenge )
       response.send( request_body.challenge );
-    } else if ( request_body.api_app_id === SLACK_BOT_APP_ID ) {
+    } else if ( request.get( 'X-Slack-Retry-Num' ) ) {
+      // This is a repeat request, ignore
+      functions.logger.info( `REPEAT REQUEST. The request has been sent ${ request.get( 'X-Slack-Retry-Num' ) } times. Reason given: ${ request.get( 'X-Slack-Retry-Reason' ) }` );
       response.sendStatus( 200 );
-      await processIssueBotRequest( request_body );
     } else {
-      const error = new Error( 'Missing/undefined or unknown api app id: ' + request.api_app_id );
-      error.code = 500;
-      throw error;
+      functions.logger.info( 'NEW REQUEST, sending immediate response back, then continuing to process it...' );
+      // Slack want's an immediate response back, and for the app to send the
+      // message to the channel through their rest api using the webhook.
+      response.write( '' );
+      if ( isIssueBotRequest( request_body ) ) {
+        await processIssueBotRequest( request_body );
+      }
+
+      response.end();
     }
   } catch ( err ) {
-    console.error( err );
-    response.sendStatus( 200 );
+    functions.logger.error( err );
+    response.end();
   }
 });
 
-async function processIssueBotRequest( request_body ) {
-  console.log( 'processIssueBotRequest' );
-  if ( request_body.type && request_body.type == 'event_callback' ) {
-    let event_response = await getEventResponse( request_body.event );
-    if ( event_response ) {
-      await sendToSlack( event_response, SLACK_WEBHOOK );
-    }
-  }
-  else {
-    const error = new Error( 'IssueBot: Missing or unknown request.body.type' );
+function isIssueBotRequest( request_body ) {
+  request_body = request_body || {};
+
+  if ( request_body.api_app_id !== SLACK_BOT_APP_ID ) {
+    const error = new Error( `Missing/undefined or unknown api app id: ${ request_body.api_app_id }` );
     error.code = 500;
     throw error;
+  }
+
+  if ( request_body.type != 'event_callback' ) {
+    functions.logger.info( `Ignoring request: request body type is not "event_callback". request_body.type: ${ request_body.type }` )
+    return false;
+  }
+
+  const event = request_body.event;
+  if ( ! event ) {
+    functions.logger.info( 'Ignoring request: request body is missing the event object.' );
+    return false;
+  }
+
+  if ( event.subtype == 'bot_message' || event.bot_id ) {
+    functions.logger.info( 'Ignoring request: request is from a bot' );
+    return false;
+  }
+
+  if ( event.type != 'message' || ! event.text ) {
+    functions.logger.info( `Ignoring request: event type isn't "message" or event text is missing ( event type: ${ event.type }, event text: ${ event.text } )` );
+    return false;
+  }
+
+  return true
+
+}
+
+async function processIssueBotRequest( request_body ) {
+  console.log( 'processIssueBotRequest' );
+  const event_response = await getEventResponse( request_body.event );
+  if ( event_response ) {
+    await sendToSlack( event_response, SLACK_WEBHOOK );
   }
 }
 
 async function getEventResponse( event ) {
   let response = '';
-  if ( event.subtype !== 'bot_message' ) {
-    if ( event.type === 'message' && event.text ) {
-      let shortCode = event.text;
-      response = await getIssueResponse( shortCode );
-    }
+  let shortCode = event.text;
+  response = await getIssueResponse( shortCode );
+
+  if ( ! response ) {
+    functions.logger.info( `No entry for short code ${ shortCode }` );
   }
 
   return response;
@@ -85,8 +122,10 @@ async function getIssues() {
 
   const issues = {};
   if ( ! res || ! res.data ) {
-    console.log( `no response or no data in response. res:`, res );
-    return issues;
+    functions.logger.error( `no response or no data in response. res:`, res );
+    const error = new Error( `Did not receive a response from Sheets API, or response did not contain a data property.` );
+    error.code = 500;
+    throw error;
   }
 
   const rows = res.data.values;
@@ -116,7 +155,7 @@ async function getAuthToken() {
 }
 
 async function sendToSlack( message, webhook ) {
-  console.log('sendToSlack message: ' + message);
+  console.log( `sendToSlack message ${ message }` );
   await webhook.send({
     text: message
   });
